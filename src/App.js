8fecf075a416d7700c7dfd9734eabe0ab3b70663
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2024-02-28 11:11:23 -0800

    Added Favorites part 1

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
+  const [favoriteQuotes, setFavoriteQuotes] = useState([]);
 
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
 
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
+  const maxFaves = 3;
+
+  
 
     const fetchQuotes = async () => {
     try{
@@ -34,15 +38,44 @@ function App() {
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
   }
-  console.log(handleCategoryChange);
+  //console.log(handleCategoryChange);
 
   const filteredQuotes = category !== "All" ? quotes.filter(quote => quote.categories.includes(category)) : quotes;
+ 
+  const addToFavorites = (quoteId) => {
+    const selectedQuote = quotes.find(quote => quote.id === quoteId)
+    const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id)
+
+    if (alreadyFavorite) {
+      console.log("You already selected this quote");
+    } else if (favoriteQuotes.length < maxFaves) {
+      setFavoriteQuotes([...favoriteQuotes, selectedQuote])
+      console.log("Added to Favorites!");
+    } else {
+      console.log("Max number of quotes reached");
+    }
+    //console.log(selectedQuote);
+  }
 
   return (
     <div className='App'>
       <Header />
       <main>
-        {loading ? <Loader /> : <Quotes filteredQuotes ={filteredQuotes} categories={categories} category = {category} handleCategoryChange={handleCategoryChange}/>}
+        <section className="favorite-quotes">
+          <div className="wrapper quotes">
+            <h3>Top 3 favorite quotes</h3>
+            {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)};
+          </div>
+        </section>
+        {loading ? 
+        <Loader /> : 
+        <Quotes 
+          filteredQuotes ={filteredQuotes} 
+          categories={categories} 
+          category = {category} 
+          handleCategoryChange={handleCategoryChange} 
+          addToFavorites={addToFavorites}
+          />}
       </main>
       <Footer />
     </div>
