39e996c929ca8affaed6478f0a270d63f951b53e
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2024-03-05 12:21:26 -0800

    commented out console logs

--- src/components/quotes/FavoriteQuoteCard.js
+++ src/components/quotes/FavoriteQuoteCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FavoriteQuoteCard = ({quote, removeFromFavorites, listPosition}) => {
-    console.log(listPosition);
+    //console.log(listPosition);
     return(
         <li className="quote-card" data-list-position={listPosition}>
             <span className="close-quote" onClick = {() => removeFromFavorites(quote.id)}>x</span>
